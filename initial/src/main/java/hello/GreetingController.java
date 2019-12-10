b0bfc79537bd45a6629c5c4ed5584ba52de1ca58
jiawei <burgessjw@163.com>
2019-12-10 13:56:41 +0800

    生成身份证号

--- initial/src/main/java/hello/GreetingController.java
+++ initial/src/main/java/hello/GreetingController.java
@@ -24,48 +24,48 @@ public class GreetingController {
 
   @RequestMapping("/idcard")
   public String idcard(@RequestParam(value="region", defaultValue="110101") String region) throws ParseException {
-  	String address = "110101";
+    String address = "110101";
 
-  	Random r = new Random();
-  	int year = r.nextInt(100) + 1900;
+    Random r = new Random();
+    int year = r.nextInt(100) + 1900;
 
     Date start = new Date(year-1900,0,1);
     Date end = new Date(year-1900,11,31);
     int days = daysBetween(start, end);
 
-	Calendar c = Calendar.getInstance(); 
-	c.setTime(start); 
-	c.add(Calendar.DATE, r.nextInt(days));
-  	SimpleDateFormat sdf = new SimpleDateFormat("yMMdd");  
-	String birthDate = sdf.format(c.getTime());
+    Calendar c = Calendar.getInstance(); 
+    c.setTime(start); 
+    c.add(Calendar.DATE, r.nextInt(days));
+    SimpleDateFormat sdf = new SimpleDateFormat("yMMdd");  
+    String birthDate = sdf.format(c.getTime());
 
-	String order = String.format("%-3s", String.valueOf(r.nextInt(999))).replace(' ', '0');
+    String order = String.format("%-3s", String.valueOf(r.nextInt(999))).replace(' ', '0');
 
-	String idCardNoWithoutChecksum = address + birthDate + order;
+    String idCardNoWithoutChecksum = address + birthDate + order;
 
-	int weight[] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
-	String marks[] = {"1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2"};
+    int weight[] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+    String marks[] = {"1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2"};
 
-	int sum = 0;
-	for (int j = 0; j<17; j++) {
+    int sum = 0;
+    char[] stringArr = idCardNoWithoutChecksum.toCharArray();
+    for (int j = 0; j<17; j++) {
+    	sum = sum + weight[j] * (int)stringArr[j];
+    }
 
-		int i = integers[j];
+    String checksum = marks[sum % 11];
 
-		System.out.println(i);
-
-	}
-
-    return idCardNoWithoutChecksum;
+    return idCardNoWithoutChecksum+checksum;
   }
 
   public static int daysBetween(Date start, Date end) throws ParseException
   {
-	Calendar cal = Calendar.getInstance();
-    cal.setTime(start);
-    long date1 = cal.getTimeInMillis();
-    cal.setTime(end);
-    long date2 = cal.getTimeInMillis();      
-    long days=(date2-date1)/(1000*3600*24);  
-    return Integer.parseInt(String.valueOf(days));
+      Calendar cal = Calendar.getInstance();
+      cal.setTime(start);
+      long date1 = cal.getTimeInMillis();
+      cal.setTime(end);
+      long date2 = cal.getTimeInMillis();      
+      long days=(date2-date1)/(1000*3600*24);  
+      
+      return Integer.parseInt(String.valueOf(days));
   }
 }
